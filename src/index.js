c361f418b48b21363ee6c7361d7120d7dc6d6498
adermanjr <aderman.jr@gmail.com>
2020-07-28 14:10:04 -0300

    slider + form

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Page404 from './pages/Page404';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-const Page404 = () => (<div>404 - Page not found!</div>)
 
 ReactDOM.render(
   <BrowserRouter>
