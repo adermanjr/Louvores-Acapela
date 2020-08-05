27b37233e2e737e1faacbe8b6413d6e96afe8b80
adermanjr <aderman.jr@gmail.com>
2020-08-05 16:58:08 -0300

    herokuapp complete url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const URL_BACKEND = window.location.href.includes('localhost') ? 'http://localhost:8080' : 'https://louvores-db.herokuapp.com';
-
+// https://louvores-db.herokuapp.com/categorias?_embed=videos
 export default {
   URL_BACKEND,
 };
